a7ed30078b1f819efaad721c11714e14867ebb27
Lianhao Song <hatsunecode@gmail.com>
2024-06-27 17:09:07 +0800

    Create Functions.r
    commit -m lhsong

---
+++ Functions.r
@@ -0,0 +1,74 @@
+#### Differential expression analysis ####
+Limma = function(expr, pos = NULL, neg = NULL, name = NULL) {
+  suppressMessages(library(limma))
+  if (!length(name)) 
+    name = paste(paste(pos, collapse = ','), 'vs', paste(neg, collapse = ',') )
+  message('DEG: ', name)
+  ## expr split
+  exprP = if (length(pos)) expr[, colnames(expr) %in% pos, drop = F] else 
+    expr[, !colnames(expr) %in% neg, drop = F]
+  exprN = if (length(neg)) expr[, colnames(expr) %in% neg, drop = F] else 
+    expr[, !colnames(expr) %in% pos, drop = F]
+  ## counts
+  exp   = cbind(exprN, exprP)
+  exp   = exp[rowSums(exp) > 0,,drop = F]
+  exprP = exp[, condition == pos, drop = F]
+  exprN = exp[, condition == neg, drop = F]
+  ## make coldata
+  coldata = data.frame(group = factor( c(rep('Neg', ncol(exprN)), rep('Pos', ncol(exprP))), c('Neg', 'Pos') ))
+  ## design
+  design = model.matrix(~group, data = coldata)
+  colnames(design) = levels(coldata$group)
+  rownames(design) = colnames(exp)
+  ## contrast
+  contrast = makeContrasts('Neg-Pos', levels = design)
+  v = voom(exp, design)
+  ## fit
+  fit  = lmFit(v, design)
+  fit2 = contrasts.fit(fit, contrast)
+  fit2 = eBayes(fit2)
+  ## output
+  dds = topTable(fit2, n = Inf)
+  data.frame(p_val = dds$P.Value, avg_log2FC = dds$logFC, 
+             pct.1 = apply(exprP, 1, function(i) sum(i > 0)/ncol(exprP) ),
+             pct.2 = apply(exprN, 1, function(i) sum(i > 0)/ncol(exprN) ),
+             p_val_adj = dds$adj.P.Val, gene = rownames(dds), average = rowMeans(expr), 
+             median = apply(expr, 1, median), 
+             posAvg = rowMeans(exprP), posMed = apply(exprP, 1, median),
+             negAvg = rowMeans(exprN), negMed = apply(exprN, 1, median),
+             type = name, row.names = NULL)
+}
+#### DESeq2
+DESeq2 = function(expr, pos = NULL, neg = NULL, name = NULL) {
+  suppressMessages(library(DESeq2))
+  if (!length(name)) 
+    name = paste(paste(pos, collapse = ','), 'vs', paste(neg, collapse = ',') )
+  message('DEG: ', name)
+  ## expr split
+  exprP = if (length(pos)) expr[, colnames(expr) %in% pos, drop = F] else 
+    expr[, !colnames(expr) %in% neg, drop = F]
+  exprN = if (length(neg)) expr[, colnames(expr) %in% neg, drop = F] else 
+    expr[, !colnames(expr) %in% pos, drop = F]
+  ## condition control ~ treatment
+  condition = factor( c(rep('Neg', ncol(exprN)), rep('Pos', ncol(exprP))), c('Neg', 'Pos') )
+  ## counts
+  expr  = cbind(exprN, exprP)
+  expr  = expr[rowSums(expr) > 0,,drop = F]
+  exprP = expr[, condition == 'Pos', drop = F]
+  exprN = expr[, condition == 'Neg', drop = F]
+  ## meta
+  meta = data.frame(row.names = colnames(expr), condition)
+  ## DESeq2
+  dds = DESeqDataSetFromMatrix(countData = counts, colData = meta, design = ~ condition)
+  dds = DESeq(dds)
+  dds = data.frame(results(dds), check.names = F)
+  ## output
+  data.frame(p_val = dds$pvalue, avg_log2FC = dds$log2FoldChange, 
+             pct.1 = apply(expP, 1, function(i) sum(i > 0)/ncol(expP) ),
+             pct.2 = apply(expN, 1, function(i) sum(i > 0)/ncol(expN) ),
+             p_val_adj = dds$padj, gene = rownames(dds), average = rowMeans(counts), 
+             median = apply(counts, 1, median), 
+             posAvg = rowMeans(expP), posMed = apply(expP, 1, median),
+             negAvg = rowMeans(expN), negMed = apply(expN, 1, median),
+             type = name, row.names = NULL)
+}
