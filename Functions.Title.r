29c3157bcc8e9c8479f20b999e50ad4db7c8b0ea
Lianhao Song <hatsunecode@gmail.com>
2024-06-29 16:41:20 +0800

    Create Functions.Title.r

---
+++ Functions.Title.r
@@ -0,0 +1,26 @@
+## process enrichment pathway title ##
+Title = function(x) {
+  subT = list(
+    'Nad '   = 'NAD ',
+    'Gnrh '  = 'GNRH ',
+    'Jak '   = 'JAK ',
+    'Ppar '  = 'PPAR ', 
+    'Vegf '  = 'VEGF ',
+    'Tgf '   = 'TGF ',
+    'Wnt '   = 'WNT ',
+    'Erbb '  = 'ERBB ',
+    'Mapk '  = 'MAPK ',
+    ' Ii '   = ' II ', 
+    ' And '  = ' and ',
+    ' Into ' = ' into ',
+    ' From ' = ' from ',
+    ' To '   = ' to ',
+    ' Of '   = ' of ',
+    ' Nadh ' = ' NADH ',
+    ' Atp '  = ' ATP '
+  )
+  x = gsub('_', ' ', sub('^WP_', '', sub('^KEGG_', '', sub('^GOBP_', '', x))))
+  x = stringr::str_to_title(x)
+  for (i in seq(subT)) x = gsub(names(subT)[i], subT[[i]], x)
+  x
+}
