40d9347550442dca0d8dbdc806e20f608fe3af1a
Lianhao Song <hatsunecode@gmail.com>
2024-07-08 17:31:02 +0800

    Update Enrich.r

--- Rfunction/Enrich.r
+++ Rfunction/Enrich.r
@@ -1,14 +1,21 @@
-## enrich: GO ##
+## Enrich: GO ##
 EnrichGO = function(genes, ont = 'BP', pval = .05, og = 'org.Mm.eg.db') {
   suppressMessages(library(clusterProfiler))
   enrichGO(suppressMessages(bitr(genes, 'SYMBOL', 'ENTREZID', og))$ENTREZID, 
            og, ont = ont, pvalueCutoff = pval, readable = T)
 }
-## fgsea ##
+
+## KEGG ##
 kegg  = msigdbr::msigdbr('Mus musculus', 'C2', 'KEGG')
 keggn = setNames(lapply(unique(kegg$gs_name), function(i)
   unique(as.character(kegg$gene_symbol)[kegg$gs_name == i])), unique(kegg$gs_name))
-##
+
+#### GO:BP ####
+bp   = msigdbr::msigdbr('Mus musculus', 'C5', 'BP')
+bpn  = setNames(lapply(unique(bp$gs_name), function(i)
+  unique(as.character(bp$gene_symbol)[bp$gs_name == i] )), unique(bp$gs_name))
+                        
+#### GSEA ####
 fGSEA = function(gene, sig, minSize = 4, maxSize = 500) {
   suppressMessages(library(fgsea))
   set.seed(1)
