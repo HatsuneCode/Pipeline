a871ecc7607261418401699b4df2756e8b6b681c
Lianhao Song <hatsunecode@gmail.com>
2024-07-08 14:28:51 +0800

    Create RNAseq.stat.r

---
+++ RNAseq/RNAseq.stat.r
@@ -0,0 +1,23 @@
+path = '/home/songlianhao/Project/ADPD/b2.bulkMut/Fastqs'
+
+fs = list.files(path, recursive = T, full.names = T, pattern = 'genes.results')
+ns = sub('.genes.results$', '', sub('.*/', '', fs))
+
+ms = c('expected_count', 'TPM', 'FPKM')
+
+data = lapply(seq(ns), function(i) {
+  f = fs[i]
+  n = ns[i]
+  message(n)
+  df = read.table(f, sep = '\t', header = T)
+  setNames(lapply(ms, function(m) {
+    setNames(data.frame(df[[m]], row.names = df$gene_id, check.rows = F), n)
+  }), ms)
+})
+
+data = lapply(ms, function(m) {
+  message(m)
+  df = do.call(cbind, lapply(data, function(i) i[[m]] ))
+  df = cbind(Gene = rownames(df), df)
+  write.table(df, paste0(m, '.xls'), sep = '\t', quote = F, row.names = F)
+})
