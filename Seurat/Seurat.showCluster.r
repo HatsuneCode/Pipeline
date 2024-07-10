426afb7518137e30c3e6700e4342f902d8eed530
Lianhao Song <hatsunecode@gmail.com>
2024-07-10 19:52:42 +0800

    Update Seurat.showCluster.r

--- Seurat/Seurat.showCluster.r
+++ Seurat/Seurat.showCluster.r
@@ -4,7 +4,7 @@ showCluster.st = function(obj, name = 'clusters', dir = name) {
   for (i in sort(unique(Idents(obj)))) {
     message('show cluster: ', i)
     p = SpatialPlot(obj, cells.highlight = Cells(obj)[Idents(obj) == i], image.alpha = 0,
-                    images = unique(obj$orig.ident), ncol = 5, alpha = .5, cols.highlight = c('red', 'white'))
+                    images = unique(obj$orig.ident), ncol = 5, alpha = .5, cols.highlight = c('red', 'grey80'))
     ggsave(paste0(dir, '/', name, '.c', i, '.png'), p, w = 4*5, h = 3*ceiling(length(unique(obj$orig.ident))/5))
   }
 }
