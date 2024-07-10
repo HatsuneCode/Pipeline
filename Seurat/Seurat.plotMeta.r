bff18ddd0ad9591fe12a0f11eeffd62ad3e1e765
Lianhao Song <hatsunecode@gmail.com>
2024-07-10 19:30:19 +0800

    Create Seurat.plotMeta.r

---
+++ Seurat/Seurat.plotMeta.r
@@ -0,0 +1,7 @@
+## plot object meta.data ##
+plotMeta = function(obj, meta, cols = NULL, nrow = 1) {
+  wrap_plots(lapply(meta, function(m)
+    VlnPlot(obj, m, pt.size = 0, cols = cols, group.by = 'slides') + labs(x = NULL) +
+      geom_boxplot(width = .2, outlier.shape = NA) + 
+      theme(legend.position = 'none', text = element_text(family = 'serif')) ), nrow = nrow)
+}
