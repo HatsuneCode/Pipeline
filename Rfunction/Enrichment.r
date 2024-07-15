8dbe6d772104c4827e6b0a71f18a7e1c43d95108
Lianhao Song <hatsunecode@gmail.com>
2024-07-15 21:29:55 +0800

    Update Enrichment.r

--- Rfunction/Enrichment.r
+++ Rfunction/Enrichment.r
@@ -5,19 +5,24 @@ EnrichGO = function(genes, ont = 'BP', pval = .05, og = 'org.Mm.eg.db') {
            og, ont = ont, pvalueCutoff = pval, readable = T)
 }
 
-## KEGG ##
-kegg  = msigdbr::msigdbr('Mus musculus', 'C2', 'KEGG')
-keggn = setNames(lapply(unique(kegg$gs_name), function(i)
-  unique(as.character(kegg$gene_symbol)[kegg$gs_name == i])), unique(kegg$gs_name))
-
-#### GO:BP ####
-bp   = msigdbr::msigdbr('Mus musculus', 'C5', 'BP')
-bpn  = setNames(lapply(unique(bp$gs_name), function(i)
-  unique(as.character(bp$gene_symbol)[bp$gs_name == i] )), unique(bp$gs_name))
-                        
 #### GSEA ####
-fGSEA = function(gene, sig, minSize = 4, maxSize = 500) {
+fGSEA = function(gene, sig = NULL, minSize = 4, maxSize = 500, type = NULL, species = 'Homo sapiens') {
+  # species: Homo sapiens / Mus musculus
   suppressMessages(library(fgsea))
+  if ('KEGG' %in% type) {
+    kegg  = msigdbr::msigdbr(species, 'C2', 'KEGG')
+    keggn = setNames(lapply(unique(kegg$gs_name), function(i)
+      unique(as.character(kegg$gene_symbol)[kegg$gs_name == i])), unique(kegg$gs_name))
+    rm(kegg)
+    sig = keggn
+  }
+  if ('GOBP' %in% type) {
+    bp   = msigdbr::msigdbr(species, 'C5', 'BP')
+    bpn  = setNames(lapply(unique(bp$gs_name), function(i)
+      unique(as.character(bp$gene_symbol)[bp$gs_name == i] )), unique(bp$gs_name))
+    rm(bp)
+    sig = bpn
+  }
   set.seed(1)
   gsea = fgsea(sig, gene, minSize = minSize, maxSize = maxSize)
   if (nrow(gsea)) {
