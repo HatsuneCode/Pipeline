28019c4888d28d1f567efcd19212ab3e4a1fbf59
Lianhao Song <hatsunecode@gmail.com>
2024-07-15 10:33:49 +0800

    Create RNAseq.checkDupRow.r

---
+++ RNAseq/RNAseq.checkDupRow.r
@@ -0,0 +1,20 @@
+checkDupRow = function(expr, method = 'mean') {
+  ## process symbols
+  gene  = sub('^ENS.*?_', '', rownames(expr))
+  dgene = unique(gene[duplicated(gene)])
+  ## check duplicated symbols
+  if (length(dgene)) {
+    expr1 = expr[!gene %in% dgene,]
+    expr2 = do.call(rbind, lapply(dgene, function(g) {
+      e = expr[gene == g, , drop = F]
+      if (method == 'mean')
+        t(setNames(data.frame(colMeans(e)), g))
+    }))
+    expr = rbind(expr1, expr2)
+    rm(expr1, expr2)
+  }
+  rm(gene, dgene)
+  ## restore symbol
+  rownames(expr) = sub('^ENS.*?_', '', rownames(expr))
+  expr
+}
