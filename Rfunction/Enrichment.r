412a36a44a83ba50737b9f2c8a252f3032fd3e73
Lianhao Song <hatsunecode@gmail.com>
2024-07-16 19:56:56 +0800

    Update Enrichment.r

--- Rfunction/Enrichment.r
+++ Rfunction/Enrichment.r
@@ -1,5 +1,6 @@
 ## Enrich: GO ##
 EnrichGO = function(genes, ont = 'BP', pval = .05, og = 'org.Mm.eg.db') {
+  ## og: org.Hs.eg.db / org.Mm.eg.db
   suppressMessages(library(clusterProfiler))
   enrichGO(suppressMessages(bitr(genes, 'SYMBOL', 'ENTREZID', og))$ENTREZID, 
            og, ont = ont, pvalueCutoff = pval, readable = T)
