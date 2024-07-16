b4f54feb1c9cc456fadfa8dc333c3e56dea32099
Lianhao Song <hatsunecode@gmail.com>
2024-07-16 17:34:44 +0800

    Update Enrichment.r

--- Rfunction/Enrichment.r
+++ Rfunction/Enrichment.r
@@ -6,7 +6,7 @@ EnrichGO = function(genes, ont = 'BP', pval = .05, og = 'org.Mm.eg.db') {
 }
 
 #### GSEA ####
-fGSEA = function(gene, sig = NULL, minSize = 4, maxSize = 500, type = NULL, species = 'Homo sapiens') {
+fGSEA = function(gene, sig = NULL, scoreType = 'std', minSize = 4, maxSize = 500, type = NULL, species = 'Homo sapiens') {
   # species: Homo sapiens / Mus musculus
   suppressMessages(library(fgsea))
   if ('KEGG' %in% type) {
@@ -24,7 +24,7 @@ fGSEA = function(gene, sig = NULL, minSize = 4, maxSize = 500, type = NULL, spec
     sig = bpn
   }
   set.seed(1)
-  gsea = fgsea(sig, gene, minSize = minSize, maxSize = maxSize)
+  gsea = fgsea(sig, gene, minSize = minSize, maxSize = maxSize, scoreType = scoreType)
   if (nrow(gsea)) {
     gsea$gene = unlist(lapply(gsea$leadingEdge, function(i) paste(i, collapse = ', ') ))
     data.frame(gsea[, c('pathway', 'NES', 'ES', 'pval', 'padj', 'gene')])
