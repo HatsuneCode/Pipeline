79ab96c9a25280e8ee5cd58c7e05bed570c488e6
Lianhao Song <hatsunecode@gmail.com>
2024-07-18 12:04:30 +0800

    Create plotST.clusters.r

---
+++ Visium/plotST.clusters.r
@@ -0,0 +1,10 @@
+## plot each clusters in ST ##
+plotST.clusters = function(obj, name = 'clusters', dir = name, ncol = 4) {
+  dir.create(dir, F)
+  for (i in sort(unique(Idents(obj)))) {
+    message('show cluster: ', i)
+    p = SpatialPlot(obj, cells.highlight = Cells(obj)[Idents(obj) == i], image.alpha = .7,
+                    images = unique(obj$orig.ident), ncol = ncol, cols.highlight = c('red', 'grey80'))
+    ggsave(paste0(dir, '/', name, '.c', i, '.png'), p, w = 4*ncol, h = 3*ceiling(length(unique(obj$orig.ident))/ncol))
+  }
+}
