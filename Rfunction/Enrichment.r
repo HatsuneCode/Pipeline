7c00b1f55c75f5e1dc6f0cdea0e734f66e0e11e1
Lianhao Song <hatsunecode@gmail.com>
2024-07-19 10:35:32 +0800

    Update Enrichment.r

--- Rfunction/Enrichment.r
+++ Rfunction/Enrichment.r
@@ -10,15 +10,16 @@ EnrichGO = function(genes, ont = 'BP', pval = .05, og = 'org.Mm.eg.db') {
 fGSEA = function(gene, sig = NULL, scoreType = 'std', minSize = 4, maxSize = 500, type = NULL, species = 'Homo sapiens') {
   # species: Homo sapiens / Mus musculus
   suppressMessages(library(fgsea))
+  suppressMessages(library(msigdbr))
   if ('KEGG' %in% type) {
-    kegg  = msigdbr::msigdbr(species, 'C2', 'KEGG')
+    kegg  = msigdbr(species, 'C2', 'KEGG')
     keggn = setNames(lapply(unique(kegg$gs_name), function(i)
       unique(as.character(kegg$gene_symbol)[kegg$gs_name == i])), unique(kegg$gs_name))
     rm(kegg)
     sig = keggn
   }
   if ('GOBP' %in% type) {
-    bp   = msigdbr::msigdbr(species, 'C5', 'BP')
+    bp   = msigdbr(species, 'C5', 'BP')
     bpn  = setNames(lapply(unique(bp$gs_name), function(i)
       unique(as.character(bp$gene_symbol)[bp$gs_name == i] )), unique(bp$gs_name))
     rm(bp)
