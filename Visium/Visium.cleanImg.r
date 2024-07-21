607c556382fff92d1114494d93d2ff1500cb5023
Lianhao Song <hatsunecode@gmail.com>
2024-07-21 17:33:06 +0800

    Create Visium.cleanImg.r

---
+++ Visium/Visium.cleanImg.r
@@ -0,0 +1,8 @@
+Visium.cleanImg = function(obj, images) {
+  ## remove otiose
+  obj@images = obj@images[intersect(names(obj@images), images)]
+  ## match cells
+  for (m in names(obj@images))
+    obj@images[[m]] = st@images[[m]][intersect(Cells(obj), Cells(obj@images[[m]])),]
+  st
+}
