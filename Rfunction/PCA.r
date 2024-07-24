90aac38380328a47883dcac21fb9ebeb2ba6eabd
Lianhao Song <hatsunecode@gmail.com>
2024-07-24 10:52:42 +0800

    Update PCA.r

--- Rfunction/PCA.r
+++ Rfunction/PCA.r
@@ -1,28 +1,8 @@
 ## PCA
-PCA = function(expr, n.varGene = 3e+3, varOut = 'PCA.nVar.png') {
-  suppressMessages(library(ggplot2))
-  suppressMessages(library(ggrepel))
-  #### var Genes
-  genes = grep('_', rownames(expr), value = T)
-  if (length(genes)) {
-    genes = data.frame(Raw = genes, Pro = gsub('_', '-', genes))
-  }
-  if (length(n.varGene)) {
-    suppressMessages(library(Seurat))
-    obj = CreateSeuratObject(expr)
-    obj = FindVariableFeatures(obj, nfeatures = n.varGene)
-    p = VariableFeaturePlot(obj)
-    if (length(varOut)) ggsave(varOut, p, w = 6, h = 5)
-    var = VariableFeatures(obj)
-    rm(obj)
-    if (length(genes)) {
-      comm = intersect(var, genes$Pro)
-      if (length(comm)) var[match(comm, var)] = genes$Raw[match(comm, genes$Pro)]
-    }
-    expr = expr[var,]
-  }
-  #### PCA
-  pca = data.frame(prcomp(t(expr))$x, check.rows = F)
+PCA = function(expr, n.varGene = 3e+3, ...) {
+  if (length(n.varGene))
+    expr = expr[RNAseq.varGene(expr, n.varGene = n.varGene, ...),]
+  pca = data.frame(prcomp(t(expr), ...)$x, check.rows = F)
   pca$sample = rownames(pca)
   pca$sample = factor(pca$sample, pca$sample)
   pca
