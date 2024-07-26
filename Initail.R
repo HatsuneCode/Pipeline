86bab95b7757741676164a6ccf1f3bb573a7c6a5
Lianhao Song <hatsunecode@gmail.com>
2024-07-26 09:35:23 +0800

    Update Initail.R

--- Initail.R
+++ Initail.R
@@ -1,6 +1,8 @@
+path = 'C:/Rcodes/Pipeline'
+
 ## r scripts ##
 lhsong = new.env()
-rs = list.files('C:/Rcodes/Pipeline/', '.r$', full.names = T, recursive = T)
+rs = list.files(path, '.r$', full.names = T, recursive = T)
 for (f in rs) { message('source: ', f); source(f, local = lhsong) }
 rm(f, rs)
 attach(lhsong)
