ca924b9c7b6e02c1ecfcc8ef4071590466a0fca8
Lianhao Song <hatsunecode@gmail.com>
2024-07-30 14:38:12 +0800

    Update 0.constant.r

--- Visium/MusBrain/0.constant.r
+++ Visium/MusBrain/0.constant.r
@@ -1,9 +1,15 @@
+## project ##
+project = 'ADPD'
+wdir    = '/home/songlianhao/Project/ADPD'
+
 ## models ##
 mods    = c('5XFAD', 'Tau', 'Gba', 'Syn', 'TDP43', 'WT28M', 'WT9M')
 mods.WC = c('WT28MWC', 'WT2MWC')
+
 ## slides ##
 slides    = unlist(lapply(mods,    function(m) paste0(m, c('.S55', '.S70', '.S83', '.S94')) ))
 slides.WC = unlist(lapply(mods.WC, function(m) paste0(m, c('.S61', '.S81', '.S89', '.S94')) ))
+
 #### color ####
 modColor = c('5XFAD'    = '#B92166', 
              'Tau'      = '#FAB158', 
