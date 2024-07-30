89b9a7adff5b42c9894e2b9f90b0ed999d321f77
Lianhao Song <hatsunecode@gmail.com>
2024-07-30 14:41:12 +0800

    Update Seurat.plotMeta.r

--- Seurat/Seurat.plotMeta.r
+++ Seurat/Seurat.plotMeta.r
@@ -1,9 +1,9 @@
 ## plot object meta.data ##
-Seurat.plotMeta = function(obj, meta, cols = NULL, nrow = 1) {
+Seurat.plotMeta = function(obj, meta, group.by = 'slides', cols = NULL, nrow = 1) {
   suppressMessages(library(Seurat))
   suppressMessages(library(patchwork))
   wrap_plots(lapply(meta, function(m)
-    VlnPlot(obj, m, pt.size = 0, cols = cols, group.by = 'slides') + labs(x = NULL) +
+    VlnPlot(obj, m, pt.size = 0, cols = cols, group.by = group.by) + labs(x = NULL) +
       geom_boxplot(width = .2, outlier.shape = NA) + 
       theme(legend.position = 'none', text = element_text(family = 'serif')) ), nrow = nrow)
 }
