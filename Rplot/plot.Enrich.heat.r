ff3672dbb44c2e7767b34d7967e7f820901b5464
Lianhao Song <hatsunecode@gmail.com>
2024-08-02 19:40:54 +0800

    Update plot.Enrich.heat.r

--- Rplot/plot.Enrich.heat.r
+++ Rplot/plot.Enrich.heat.r
@@ -2,16 +2,16 @@ plot.Enrich.heat = function(df, nes = 2, pval = .05, adj = T, label = T, grid = 
   suppressMessages(library(reshape2))
   suppressMessages(library(ComplexHeatmap))
   suppressMessages(library(circlize))
-  nes = acast(df, pathway ~ group, value.var = 'NES',  fill = 0)
-  pvl = acast(df, pathway ~ group, value.var = if (adj) 'padj' else 'pval', fill = 1)
+  Nes = acast(df, pathway ~ group, value.var = 'NES',  fill = 0)
+  Pvl = acast(df, pathway ~ group, value.var = if (adj) 'padj' else 'pval', fill = 1)
   ff  = gpar(fontfamily = 'serif')
-  p   = Heatmap(nes, name = 'NES',
+  p   = Heatmap(Nes, name = 'NES',
                 col = colorRamp2(c(-nes, 0, nes), c('blue', 'white', 'red')), 
                 rect_gp = gpar(col = 'grey80'),
-                row_names_side = 'left', show_row_dend = F, row_names_gp = ff, row_names_max_width = max_text_width(rownames(nes)), 
+                row_names_side = 'left', show_row_dend = F, row_names_gp = ff, row_names_max_width = max_text_width(rownames(Nes)), 
                 cluster_columns = F, column_names_side = 'top', column_names_gp = ff,
                 cell_fun = function(j, i, x, y, w, h, col) {
-                  f = nes[i, j]; p = pvl[i, j]
+                  f = Nes[i, j]; p = Pvl[i, j]
                   if (label) grid.text(round(f, 2), x, y, gp = ff)
                   if (grid & p < pval) 
                     grid.rect(x, y, w, h,
