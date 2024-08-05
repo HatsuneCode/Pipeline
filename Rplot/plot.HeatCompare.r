1e552911dde7f0b5eefff2ce6fd7fc9b6795d88a
Lianhao Song <hatsunecode@gmail.com>
2024-08-05 14:14:23 +0800

    Update plot.HeatCompare.r

--- Rplot/plot.HeatCompare.r
+++ Rplot/plot.HeatCompare.r
@@ -1,5 +1,5 @@
 ## pList: ComplexHeatmap plots list; pvList: pvalue matrixs list
-plot.HeatCompare = function(pList, pvList, fc = 1, pval = .01) {
+plot.HeatCompare = function(pList, pvList = NULL, fc = 1, pval = .01, upCol = 'red', downCol = 'blue') {
   mtrs = lapply(pList, function(p) p@matrix )
   rows = lapply(seq(mtrs), function(i) rownames(mtrs[[i]])[row_order(draw(pList[[i]]))] )
   all  = unique(unlist(rows))
@@ -7,10 +7,9 @@ plot.HeatCompare = function(pList, pvList, fc = 1, pval = .01) {
   for (i in seq(mtrs)) {
     dif = setdiff(all, rows[[i]])
     if (length(dif)) {
-      pList[[i]]@matrix = rbind(mtrs[[i]], matrix(0, nrow = length(dif), ncol = ncol(mtrs[[i]]), 
-                                dimnames = list(dif, colnames(mtrs[[i]]))) )[all,]
-      pvList[[i]] = rbind(pvList[[i]], matrix(1, nrow = length(dif), ncol = ncol(pvList[[i]]), 
-                                dimnames = list(dif, colnames(pvList[[i]]))) )[all,]
+      pList[[i]]@matrix = rbind(mtrs[[i]], matrix(0, nrow = length(dif), ncol = ncol(mtrs[[i]]), dimnames = list(dif, colnames(mtrs[[i]]))) )[all,]
+      if (length(pvList))
+        pvList[[i]] = rbind(pvList[[i]], matrix(1, nrow = length(dif), ncol = ncol(pvList[[i]]), dimnames = list(dif, colnames(pvList[[i]]))) )[all,]
     }
   }
   ## plots
@@ -18,13 +17,13 @@ plot.HeatCompare = function(pList, pvList, fc = 1, pval = .01) {
     p = pList[[n]]
     p@row_order = seq(all)
     mtr = p@matrix
-    pvl = pvList[[n]]
+    if (length(pvList)) pvl = pvList[[n]]
     p@matrix_param$cell_fun = function(j, i, x, y, w, h, col) {
       f = mtr[i, j]; p = pvl[i, j]
-      if (abs(f) > fc & p < pval)
-        grid.rect(x, y, w, h, gp = gpar(fill = 'transparent', lwd = 1.5, col = ifelse(f > 0, 'red', 'blue')))
+      if (length(pvList)) if (abs(f) > fc & p < pval)
+        grid.rect(x, y, w, h, gp = gpar(fill = 'transparent', lwd = 1.5, col = ifelse(f > 0, upCol, downCol)))
     }
-    p@row_names_param$anno@var_env$value = rownames(pList[[n]]@matrix)
+    p@row_names_param$anno@var_env$value = rownames(mtr)
     p@row_dend_param$cluster = F
     p@row_dend_param$show = F
     p
