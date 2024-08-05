9feeb2f5b469ec6a7c2811f1de3d4def9afbed57
Lianhao Song <hatsunecode@gmail.com>
2024-08-05 16:44:24 +0800

    Update plot.Boxplot.r

--- Rplot/plot.Boxplot.r
+++ Rplot/plot.Boxplot.r
@@ -8,14 +8,16 @@ plot.Boxplot = function(expr, gene, group = NULL, compare = NULL, cols = NULL) {
   p   = ggplot(exp, aes(Group, Expr)) + 
     geom_boxplot(aes(fill = Group), outlier.shape = NA, show.legend = F) +
     geom_point(aes(color = Group), show.legend = F) +
-    stat_compare_means(comparisons = compare, label = 'p.signif') +
     labs(x = NULL, y = 'Normalized Expression', title = gene) +
     theme_classic() +
     theme(text = element_text(family = 'serif', size = 14), 
           axis.text.x = element_text(angle = 45, hjust = 1),
-          plot.title = element_text(hjust = .5)) +
-    if (length(cols)) 
-      scale_fill_manual(values = cols) + scale_color_manual(values = cols)
-  p$layers[[3]]$aes_params$family = 'serif'
+          plot.title = element_text(hjust = .5))
+  if (length(compare)) {
+    p = p + stat_compare_means(comparisons = compare, label = 'p.signif')
+    p$layers[[3]]$aes_params$family = 'serif'
+  }
+  if (length(cols))
+    p = p + scale_fill_manual(values = cols) + scale_color_manual(values = cols)
   p
 }
