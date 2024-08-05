b1543e1c00639aa89fb8909eef86975faf633e26
Lianhao Song <hatsunecode@gmail.com>
2024-08-05 20:50:24 +0800

    Update cleanGene.r

--- Rfunction/cleanGene.r
+++ Rfunction/cleanGene.r
@@ -1,6 +1,6 @@
 ## Remove unused genes ##
 cleanGene = function(x, cleanMT = T, value = T) {
-  idx = !(grepl('^R[P,p][L,l,S,s][0-9,P,p]', x) | grepl('^ENS[M,G]', x) | 
+  idx = !(grepl('^R[P,p][L,l,S,s][0-9,P,p]', x) | grepl('^ENS[M,G]', x) | grepl('^AI[0-9][0-9][0-9]', x) |
             grepl('^Gm[0-9]', x) | grepl('^R[N,n][0-9]', x) | grepl('Rik', x) | grepl('^H[B,b].-', x) |
             grepl('^A[A,C,W][0-9]', x) | grepl('^mt-T|^MT-T', x) | grepl('^LINC[0-9]', x) )
   if (cleanMT) idx = idx & !grepl('^mt-|^MT-', x)
