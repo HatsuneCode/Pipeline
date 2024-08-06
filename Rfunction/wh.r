6f960756a2c24cd09dcd23edb6260cebfc0c1161
Lianhao Song <hatsunecode@gmail.com>
2024-08-06 17:44:02 +0800

    Update and rename Rfunction/GSEA/GSEA.wh.r to Rfunction/wh.r

--- Rfunction/wh.r
+++ Rfunction/wh.r
@@ -1,4 +1,4 @@
-GSEA.wh = function(df, row = 'pathway', col = 'group') {
+wh = function(df, row, col) {
   w = as.numeric(max_text_width(df[[row]])/25.4) + length(unique(df[[col]]))*.2 + .3
   h = as.numeric(max_text_width(df[[col]])/25.4) + length(unique(df[[row]]))*.15
   list(w = w, h = h)
