5411349297428d8e85464c645363f9b81e5bdb63
Lianhao Song <hatsunecode@gmail.com>
2024-08-08 14:35:18 +0800

    Update wh.r

--- Rfunction/wh.r
+++ Rfunction/wh.r
@@ -1,5 +1,5 @@
 wh = function(df, row, col) {
-  w = as.numeric(max_text_width(df[[row]])/25.4) + length(unique(df[[col]]))*.2 + .3
-  h = as.numeric(max_text_width(df[[col]])/25.4) + length(unique(df[[row]]))*.15
+  w = as.numeric(max_text_width(df[[row]])/25.4) + length(unique(df[[col]]))*.2 + .5
+  h = as.numeric(max_text_width(df[[col]])/25.4) + length(unique(df[[row]]))*.15 + .3
   list(w = w, h = h)
 }
