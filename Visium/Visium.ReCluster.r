d2e38c4e88a22d08842b74fa2ac2deae61195506
Lianhao Song <hatsunecode@gmail.com>
2024-08-09 10:18:23 +0800

    Update Visium.ReCluster.r

--- Visium/Visium.ReCluster.r
+++ Visium/Visium.ReCluster.r
@@ -1,46 +1,18 @@
 ## reCluster
-Visium.ReCluster = function(obj, group.by = 'slides', assay = 'ST', cluster = T, ctrl = NULL) {
+Visium.ReCluster = function(obj, harmony = T, group.by = 'slides', dims = 1:50) {
   suppressMessages(library(Seurat))
   suppressMessages(library(harmony))
   #### SCT
   idx = obj@meta.data[[group.by]]
-  obj = lapply(unique(idx), function(m) {
-    m = as.character(m)
-    message('SCT: ', m)
-    st = obj[, idx == m]
-    st = Visium.cleanImg(st, names(obj@images))
-    st = SCTransform(st, assay, vst.flavor = 'v2', vars.to.regress = c('mt.pct', 'cc.diff', 'nCount_ST') )
-    list(obj = st, feature = VariableFeatures(st))
-  })
-  #### merge
-  feature = lapply(obj, function(i) i[[2]] )
-  obj     = lapply(obj, function(i) i[[1]] )
-  imgs    = make.unique(sapply(obj, function(i) names(i@images) ))
-  obj     = lapply(obj, function(i) {
-    names(i@images) = paste0('New.', names(i@images)); i })
-  if (length(obj)-1) {
-    message('--> merge... <--'); obj = merge(obj[[1]], obj[-1])
-  } else obj = obj[[1]]
-  names(obj@images) = imgs
-  obj = Visium.cleanImg(obj, imgs)
-  #### restore
-  VariableFeatures(obj) = unique(if (length(ctrl)) 
-    cleanGene( unlist(feature[grepl(ctrl, unique(idx))]) ) else 
-      cleanGene( unlist(feature)) )
-  message('--> nVarGene: ', length(VariableFeatures(obj)), ' <--')
-  obj = PrepSCTFindMarkers(obj)
-  #### Cluster
-  if (cluster) {
-    obj = RunPCA(obj, verbose = F)
-    if (length(unique(idx))-1) {
-      obj = RunHarmony(obj, group.by)
-      obj = RunUMAP(obj, dims = 1:50, reduction = 'harmony')
-      obj = FindNeighbors(obj, dims = 1:50, reduction = 'harmony')
-    } else {
-      obj = RunUMAP(obj, dims = 1:50)
-      obj = FindNeighbors(obj, dims = 1:50)
-    }
-    obj = FindClusters(obj)
+  obj = RunPCA(obj, verbose = F)
+  if (harmony & length(unique(idx))-1) {
+    obj = RunHarmony(obj, group.by)
+    obj = RunUMAP(obj, dims = dims, reduction = 'harmony')
+    obj = FindNeighbors(obj, dims = dims, reduction = 'harmony')
+  } else {
+    obj = RunUMAP(obj, dims = dims)
+    obj = FindNeighbors(obj, dims = dims)
   }
+  obj = FindClusters(obj)
   obj
 }
