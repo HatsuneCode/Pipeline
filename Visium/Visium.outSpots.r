8ac6c2655aa330c64a3b715f09cafd1eeb77606c
Lianhao Song <hatsunecode@gmail.com>
2024-08-10 17:55:13 +0800

    Update Visium.outSpots.r

--- Visium/Visium.outSpots.r
+++ Visium/Visium.outSpots.r
@@ -1,12 +1,14 @@
 ## output spots ##
-Visium.outSpots = function(obj, name = 'SPOTout', outdir = name) {
+Visium.outSpots = function(obj, split = 'slides', group = NULL, name = 'SPOTout', outdir = name) {
+  idx = if (!length(group)) Idents(obj) else obj@meta.data[[group]]
   dir.create(outdir, F)
-  lapply(unique(obj$slides), function(s) {
-    message('--> out: ', s, ' to ', outdir, ' <--')
-    cells = sub(paste0('^', s, '_'), '', Cells(obj)[obj$slides == s])
-    write.csv(data.frame(Barcode = cells, Spot = s), 
-              paste0(outdir, '/', name, '-', gsub('/', '', s), '.cells.csv'),
-              row.names = F, quote = F)
+  lapply(unique(obj@meta.data[[split]]), function(s) {
+    s = as.character(s)
+    m = obj@meta.data[[split]] == s
+    message('--> outSpots: ', s, ' to ', outdir, ' <--')
+    cells = sub(paste0('^', s, '_'), '', Cells(obj)[m])
+    write.csv(setNames(data.frame(cells, as.character(idx)[m]), c('Barcode', name)), 
+              paste0(outdir, '/', name, '_', gsub('/', '', s), '.cells.csv'), row.names = F, quote = F)
   })
   message('--> done <--')
 }
