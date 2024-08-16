10d3db0050c8137bc467a4103678e8c7b1a06819
Lianhao Song <hatsunecode@gmail.com>
2024-08-16 16:15:29 +0800

    Create Seurat.Cluster.r

---
+++ Seurat/Seurat.Cluster.r
@@ -0,0 +1,18 @@
+## reCluster
+Seurat.Cluster = function(obj, harmony = T, group.by = 'slides', dims = 1:50, reso = .8) {
+  suppressMessages(library(Seurat))
+  suppressMessages(library(harmony))
+  #### SCT
+  idx = obj@meta.data[[group.by]]
+  obj = RunPCA(obj, verbose = F)
+  if (harmony & length(unique(idx))-1) {
+    obj = RunHarmony(obj, group.by)
+    obj = RunUMAP(obj, dims = dims, reduction = 'harmony')
+    obj = FindNeighbors(obj, dims = dims, reduction = 'harmony')
+  } else {
+    obj = RunUMAP(obj, dims = dims)
+    obj = FindNeighbors(obj, dims = dims)
+  }
+  obj = FindClusters(obj, resolution = reso)
+  obj
+}
