17f3e3e78a2911bedd57547dc5ef47e116590fe3
Lianhao Song <hatsunecode@gmail.com>
2024-08-23 15:16:59 +0800

    Update plot.GSEA.heat.r

--- Rplot/plot.GSEA.heat.r
+++ Rplot/plot.GSEA.heat.r
@@ -1,16 +1,18 @@
-plot.GSEA.heat = function(df, nes = 2, pval = .05, adj = T, label = T, grid = T, ...) {
+plot.GSEA.heat = function(df, nes = 2, pval = .05, adj = T, label = T, grid = T, t = F, ...) {
   suppressMessages(library(reshape2))
   suppressMessages(library(ComplexHeatmap))
   suppressMessages(library(circlize))
   Nes = acast(df, pathway ~ group, value.var = 'NES',  fill = 0)
   Pvl = acast(df, pathway ~ group, value.var = if (adj) 'padj' else 'pval', fill = 1)
+  if (t) { Nes = t(Nes); Pvl = t(Pvl) }
   ff  = gpar(fontfamily = 'serif')
   p   = Heatmap(Nes, name = 'NES',
                 col = colorRamp2(c(-nes, 0, nes), c('blue', 'white', 'red')), 
                 rect_gp = gpar(col = 'grey80'),
-                row_names_side = 'left', show_row_dend = F, row_names_gp = ff, row_title_gp = ff,
-                row_names_max_width = max_text_width(rownames(Nes)), 
-                cluster_columns = F, column_names_side = 'top', column_names_gp = ff, column_title_gp = ff, 
+                row_names_side = 'left', row_names_gp = ff, row_title_gp = ff,
+                row_names_max_width = max_text_width(rownames(Nes)), column_names_max_height = max_text_width(colnames(Nes)),
+                cluster_columns = t, cluster_rows = !t, show_column_dend = F, show_row_dend = F,
+                column_names_side = 'top', column_names_gp = ff, column_title_gp = ff, 
                 cell_fun = function(j, i, x, y, w, h, col) {
                   f = Nes[i, j]; p = Pvl[i, j]
                   if (label) grid.text(round(f, 2), x, y, gp = ff)
