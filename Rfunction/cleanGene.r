f4a4a389844e002612834897f9984dbd3ce7664e
Lianhao Song <hatsunecode@gmail.com>
2024-08-31 11:19:52 +0800

    Update cleanGene.r

--- Rfunction/cleanGene.r
+++ Rfunction/cleanGene.r
@@ -5,6 +5,6 @@ cleanGene = function(x, cleanMT = T, cleanSex = T, value = T) {
             grepl('^Gm[0-9]', x) | grepl('^R[N,n][0-9]', x) | grepl('Rik', x) | grepl('^H[B,b].-', x) |
             grepl('^A[A,C,W][0-9]', x) | grepl('^mt-T|^MT-T', x) | grepl('^LINC[0-9]', x) )
     if (cleanMT)  idx = idx & !grepl('^mt-|^MT-', x)
-    if (cleanSex) idx = idx & ! x %in% c('Xist', 'XIST', 'Ddx3y', 'DDX3Y', 'Eif2s3y', 'EIF2S3Y', 'Uty', 'UTY', 'Kdm5d', 'KDM5D')
+    if (cleanSex) idx = idx & !x %in% c('Xist', 'XIST', 'Ddx3y', 'DDX3Y', 'Eif2s3y', 'EIF2S3Y', 'Uty', 'UTY', 'Kdm5d', 'KDM5D')
     if (value) x[idx] else idx
 }
