f9ac7061486fd6420862839fd40ed1d35aa5b15a
Lianhao Song <hatsunecode@gmail.com>
2024-10-29 15:45:11 +0800

    Update Enrichment.r

--- Rfunction/Enrichment.r
+++ Rfunction/Enrichment.r
@@ -2,11 +2,11 @@
 Enrich = function(genes, ont = NULL, pval = .05, og = 'org.Mm.eg.db') {
   ## og: org.Hs.eg.db / org.Mm.eg.db
   suppressMessages(library(clusterProfiler))
-  entrez_ids = suppressMessages(bitr(genes, 'SYMBOL', 'ENTREZID', og))$ENTREZID
+  ids = suppressMessages(bitr(genes, 'SYMBOL', 'ENTREZID', og))$ENTREZID
   if ('GOBP' %in% ont) {
-    enrichGO(entrez_ids, og, ont = 'BP', pvalueCutoff = pval, readable = T)@result
+    enrichGO(ids, og, ont = 'BP', pvalueCutoff = pval, readable = T)@result
   }
   if ('KEGG' %in% ont){
-    enrichKEGG(entrez_ids, organism = ifelse(og == 'org.Mm.eg.db', 'mmu', 'hsa'))@result
+    enrichKEGG(ids, organism = ifelse(og == 'org.Mm.eg.db', 'mmu', 'hsa'))@result
   }
 }
