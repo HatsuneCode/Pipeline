d56b5bb5eda9aed2ce3279f4e2b435acf803cba3
Lianhao Song <hatsunecode@gmail.com>
2024-11-01 16:59:28 +0800

    Update Initail.R

--- Initail.R
+++ Initail.R
@@ -1,9 +1,11 @@
-path = 'C:/Rcodes/Pipeline'
-
-## r scripts ##
-lhsong = new.env()
-rs = list.files(path, '.r$', full.names = T, recursive = T)
-for (f in rs) { message('source: ', f); source(f, local = lhsong) }
-rm(f, rs)
-attach(lhsong)
-rm(lhsong)
+path = 'C:/Hatsune/Pipeline'
+load = scan()
+if (1 %in% load) {
+  ## r scripts ##
+  lhsong = new.env()
+  rs = list.files(path, '.r$', full.names = T, recursive = T)
+  for (f in rs) { message('source: ', f); source(f, local = lhsong) }
+  rm(f, rs)
+  attach(lhsong)
+  rm(lhsong)
+}
