3a5b12080f750315d510e9e8964aab2ecbc5c8ae
Lianhao Song <hatsunecode@gmail.com>
2024-11-09 16:39:44 +0800

    Update plot.Boxplot.r

--- Rplot/plot.Boxplot.r
+++ Rplot/plot.Boxplot.r
@@ -1,4 +1,4 @@
-plot.Boxplot = function(expr, gene, group = NULL, compare = NULL, cols = NULL, label = 'p.signif', method = 't.test') {
+plot.Boxplot = function(expr, gene, group = NULL, compare = NULL, cols = NULL, pt.size = 1, label = 'p.signif', method = 't.test') {
   ## group: findInList(colnames(expr), pair)
   ## compare: list( c('pos1', 'neg1'), c('pos2', 'neg2') )
   ## label: p.signif, p.format
@@ -8,7 +8,7 @@ plot.Boxplot = function(expr, gene, group = NULL, compare = NULL, cols = NULL, l
   exp = data.frame(Expr = as.numeric(expr[gene,]), Group = group)
   p   = ggplot(exp, aes(Group, Expr)) + 
     geom_boxplot(aes(fill = Group), outlier.shape = NA, show.legend = F) +
-    geom_point(show.legend = F) +
+    geom_point(show.legend = F, size = pt.size) +
     labs(x = NULL, y = 'Normalized Expression', title = gene) +
     theme_classic() +
     theme(text = element_text(family = 'serif', size = 14), 
