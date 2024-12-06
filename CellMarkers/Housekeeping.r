ef2dc8f80863822f2dc32a8af7818b8cea6b9700
Lianhao Song <hatsunecode@gmail.com>
2024-12-06 16:59:31 +0800

    Update Housekeeping.r

--- CellMarkers/Housekeeping.r
+++ CellMarkers/Housekeeping.r
@@ -1,5 +1,5 @@
 ## Mice:
-housekeeping.mus = c('Gapdh', 'Actb', 'Actg1', 'Ppia', 'Pgam1', 'Hprt')
+housekeeping.mus = c('Gapdh', 'Actb', 'Actg1', 'Ppia', 'Pgam1', 'Hprt', 'Tbp', 'Oaz1', 'Aldoa', 'M6pr', 'Usp11')
 
 ## Human:
 housekeeping.hsa = c('GAPDH', 'ACTB')
