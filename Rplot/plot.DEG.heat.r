37eae477c5fcd4f31dadc1d86c22411fa52f519f
Lianhao Song <hatsunecode@gmail.com>
2024-12-15 16:13:49 +0800

    Update plot.DEG.heat.r

--- Rplot/plot.DEG.heat.r
+++ Rplot/plot.DEG.heat.r
@@ -12,7 +12,7 @@ plot.DEG.heat = function(df, log2FC = 1, log2FC.col = log2(1.2), pval = .05, adj
   pv = acast(df, gene ~ group, value.var = if (adj) 'p_val_adj' else 'p_val', fill = 1, drop = F)
   p  = Heatmap(fc, name = 'Log2FC',
                col = colorRamp2(c(-log2FC, 0, log2FC), c('blue', 'white', 'red')),
-               row_names_side = 'left', row_dend_side = 'right', row_names_gp = ff, row_title_gp = ff, 
+               row_names_side = 'left', row_dend_side = 'right', row_title_gp = ff, 
                row_names_max_width = max_text_width(rownames(fc)), column_names_max_height = max_text_width(colnames(fc)),
                cluster_columns = F, column_names_side = 'top', column_names_gp = ff, column_title_gp = ff,
                cell_fun = function(j, i, x, y, w, h, col) {
