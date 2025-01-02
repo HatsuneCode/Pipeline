bc7e2c37c1c40c59db52bec42e0c0ccc63ab1fae
Lianhao Song <hatsunecode@gmail.com>
2025-01-02 08:53:15 +0800

    Update cleanGene.r

--- Rfunction/cleanGene.r
+++ Rfunction/cleanGene.r
@@ -1,10 +1,25 @@
-## Remove unused genes ##
-cleanGene = function(x, cleanMT = T, cleanSex = T, value = T) {
-    idx = !(grepl('^R[P,p][L,l,S,s][0-9,P,p]', x) | grepl('^ENS[M,G]', x) |
-            grepl('^AI[0-9][0-9][0-9]', x) | grepl('^B[B,C][0-9][0-9][0-9]', x) |
-            grepl('^Gm[0-9]', x) | grepl('^R[N,n][0-9]', x) | grepl('Rik', x) | grepl('^H[B,b].-', x) |
-            grepl('^A[A,C,W][0-9]', x) | grepl('^mt-T|^MT-T', x) | grepl('^LINC[0-9]', x) )
-    if (cleanMT)  idx = idx & !grepl('^mt-|^MT-', x)
-    if (cleanSex) idx = idx & !x %in% c('Xist', 'XIST', 'Ddx3y', 'DDX3Y', 'Eif2s3y', 'EIF2S3Y', 'Uty', 'UTY', 'Kdm5d', 'KDM5D')
-    if (value) x[idx] else idx
+cleanGene = function(x, cleanMT = F, cleanSex = T, value = F) {
+  idx = !(
+    ## clean ribosomal gene
+    grepl('^R[P,p][L,l,S,s][0-9,P,p]', x) |
+      ## clean unclassified gene
+      grepl('^ENS[M,G]', x) |
+      grepl('^A[A,C,W][0-9]', x) | 
+      grepl('^AI[0-9][0-9][0-9]', x) | 
+      grepl('^B[B,C][0-9][0-9][0-9]', x) |
+      grepl('^Gm[0-9]', x) |
+      ## clean non-coding RNA
+      grepl('^R[N,n][0-9]', x) | 
+      grepl('Rik', x) | 
+      grepl('^LINC[0-9]', x) |
+      ## clean pseudogene
+      grepl('-ps', x) |
+      ## clean Hemoglobin
+      grepl('^H[B,b].-', x) |
+      ## clean mt-TRNA
+      grepl('^mt-T|^MT-T', x) )
+  if (cleanMT)  idx = idx & !grepl('^mt-|^MT-', x)
+  if (cleanSex) idx = idx & !x %in% 
+      c('Xist', 'XIST', 'Ddx3y', 'DDX3Y', 'Eif2s3y', 'EIF2S3Y', 'Uty', 'UTY', 'Kdm5d', 'KDM5D')
+  if (value) x[idx] else idx
 }
