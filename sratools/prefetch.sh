7f410fb6e350b0173a19c49ee982a003d64ff168
jhbioinformlab <141522490+jhbioinformlab@users.noreply.github.com>
2025-03-26 10:08:49 +0800

    Update prefetch.sh

--- sratools/prefetch.sh
+++ sratools/prefetch.sh
@@ -1,1 +1,5 @@
+#！/bin/bash
+sra_list_file="F:\xie_task\bioinfo\SRR_Acc_List.txt"
+download_to_folder="F:\xie_task\bioinfo\datasets"
 
+prefetch -p -f no --option-file ${sra_list_file} -O ${download_to_folder}
