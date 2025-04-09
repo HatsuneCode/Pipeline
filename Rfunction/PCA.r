a7557d14f3953b99449bddb6a1d2d7785fca3855
Lianhao Song <hatsunecode@gmail.com>
2025-04-09 17:50:36 +0800

    Update PCA.r

--- Rfunction/PCA.r
+++ Rfunction/PCA.r
@@ -1,7 +1,7 @@
 ## PCA
 PCA = function(expr, ...) {
   pca = data.frame(prcomp(t(expr), ...)$x, check.rows = F)
-  pca$sample = rownames(pca)
+  pca$sample = rownames(expr)
   pca$sample = factor(pca$sample, pca$sample)
   pca
 }
