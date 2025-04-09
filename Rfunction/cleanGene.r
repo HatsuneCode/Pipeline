e62c3c8c9774cfcb7afa58dc86e0c3c45fb897ff
Lianhao Song <hatsunecode@gmail.com>
2025-04-09 16:19:49 +0800

    Update cleanGene.r

--- Rfunction/cleanGene.r
+++ Rfunction/cleanGene.r
@@ -1,13 +1,14 @@
-cleanGene = function(x, cleanMT = F, cleanSex = T, value = F) {
+cleanGene = function(x, cleanMT = T, cleanSex = T, value = T) {
   idx = !(
     ## clean ribosomal gene
     grepl('^R[P,p][L,l,S,s][0-9,P,p]', x) |
       ## clean unclassified gene
       grepl('^ENS[M,G]', x) |
-      grepl('^A[A,C,W][0-9]', x) | 
-      grepl('^AI[0-9][0-9][0-9]', x) | 
+      grepl('^A[A-Z][0-9][0-9]', x) | 
       grepl('^B[B,C][0-9][0-9][0-9]', x) |
       grepl('^Gm[0-9]', x) |
+      ## clean DNA segment
+      grepl('^D[0-9][0-9,A-Z]', x) |
       ## clean non-coding RNA
       grepl('^R[N,n][0-9]', x) | 
       grepl('Rik', x) | 
@@ -18,7 +19,9 @@ cleanGene = function(x, cleanMT = F, cleanSex = T, value = F) {
       grepl('^H[B,b].-', x) |
       ## clean mt-TRNA
       grepl('^mt-T|^MT-T', x) )
+  ## clean mitochondrial gene
   if (cleanMT)  idx = idx & !grepl('^mt-|^MT-', x)
+  ## clean sex related gene
   if (cleanSex) idx = idx & !x %in% 
       c('Xist', 'XIST', 'Ddx3y', 'DDX3Y', 'Eif2s3y', 'EIF2S3Y', 'Uty', 'UTY', 'Kdm5d', 'KDM5D')
   if (value) x[idx] else idx
