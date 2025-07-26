add5454982a9f26e8851c969f652454b34975eb4
Lianhao Song <hatsunecode@gmail.com>
2025-07-26 15:49:20 +0800

    Update and rename blast.sh to blast.sh

--- blast/blast.sh
+++ blast/blast.sh
@@ -1,3 +1,5 @@
 ## download from NCBI nt library ##
 ## https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz
 ## https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz.md5
+# mkdir nt
+# makeblastdb -in nt -dbtype nucl -out nt/nt.db
