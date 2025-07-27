0aea976c3bfe1004e856b316702bb6de032582e4
Lianhao Song <hatsunecode@gmail.com>
2025-07-27 18:40:21 +0800

    Update blast.sh

--- blast/blast.sh
+++ blast/blast.sh
@@ -1,5 +1,6 @@
 ## download from NCBI nt library ##
 ## https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz
 ## https://ftp.ncbi.nlm.nih.gov/blast/db/FASTA/nt.gz.md5
+# pigz -d -k -p 16 nt.gz -c > /mnt/e/NCBI.NT.20240208/nt.fasta
 # mkdir nt
-# makeblastdb -in nt -dbtype nucl -out nt/nt.db
+# makeblastdb -in nt.fasta -dbtype nucl -out nt/nt.db
