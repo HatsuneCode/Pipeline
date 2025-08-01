1ee6b5fc9aa2f45ef8489d3e186d4b681c571ce3
Lianhao Song <hatsunecode@gmail.com>
2025-08-01 10:36:16 +0800

    Update blast.sh

--- blast/blast.sh
+++ blast/blast.sh
@@ -4,3 +4,6 @@
 # pigz -d -k -p 16 nt.gz -c > /mnt/e/NCBI.NT.20240208/nt.fasta
 # mkdir nt
 # makeblastdb -in nt.fasta -dbtype nucl -out nt/nt.db
+
+## also can remote online ##
+blastn -db nt -remote -query unmap.fa -num_threads 16
