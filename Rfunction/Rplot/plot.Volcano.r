ada6fb0c1fe41c8676bc95dca4a3d3d751b84bec
Lianhao Song <hatsunecode@gmail.com>
2026-01-29 17:16:59 +0800

    Update plot.Volcano.r

--- Rfunction/Rplot/plot.Volcano.r
+++ Rfunction/Rplot/plot.Volcano.r
@@ -1,5 +1,6 @@
 ## df need: avg_log2FC, p_val_adj, gene
-plot_FP = function(df, logFC = 1, padj = .01, label.logFC = 1.5, exprAvg = 0, 
+## df need: avg_log2FC, p_val_adj, gene
+plot_FP = function(df, FC.col = 'avg_log2FC', logFC = 1, padj = .01, label.logFC = 1.5, exprAvg = 0, 
                    xlab = 'Log2 Fold Change', title = 'DEG Log2FC-Padj', 
                    adj = T, pmax = 1e-300, 
                    label = F, label.name = NULL, label.color = 'red',
@@ -8,7 +9,7 @@ plot_FP = function(df, logFC = 1, padj = .01, label.logFC = 1.5, exprAvg = 0,
   suppressMessages(library(ggrepel))
   if (!adj) df$p_val_adj = df$p_val
   ## annot
-  df$annot = ifelse(df$avg_log2FC > logFC, 'Up', ifelse(df$avg_log2FC < -logFC, 'Down', 'NS'))
+  df$annot = ifelse(df[[FC.col]] > logFC, 'Up', ifelse(df[[FC.col]] < -logFC, 'Down', 'NS'))
   df$annot[df$p_val_adj > padj | is.na(df$p_val_adj)] = 'NS'
   df$annot[df$average < exprAvg] = 'NS'
   df$annot[df$annot == 'Up']     = paste0('Up (', table(df$annot)['Up'], ')')
@@ -16,16 +17,17 @@ plot_FP = function(df, logFC = 1, padj = .01, label.logFC = 1.5, exprAvg = 0,
   df$annot[df$annot == 'NS']     = paste0('NS (', table(df$annot)['NS'], ')')
   df$annot = factor(df$annot, sort(unique(df$annot), T))
   df$lable = if (label) if (length(label.name)) df$gene %in% label.name else 
-    abs(df$avg_log2FC) > label.logFC & !grepl('^NS', df$annot) else NA
+    abs(df[[FC.col]]) > label.logFC & !grepl('^NS', df$annot) else NA
   ## color
   annot = levels(df$annot)
   idx   = sapply(c('Up', 'NS', 'Down'), function(i) sum(grepl(i, annot)) )
   color = setNames(c(if (idx[1]) 'red', if (idx[2]) 'black', if (idx[3]) 'blue') , annot)
   ## limit
-  lim = max(abs(df$avg_log2FC))
+  lim = max(abs(df[[FC.col]]))
   df$p_val_adj[ -log10(df$p_val_adj) > -log10(pmax) ] = pmax
+  df$plotFP = df[[FC.col]]
   ## plot
-  p = ggplot(df, aes(avg_log2FC, -log10(p_val_adj))) + 
+  p = ggplot(df, aes(plotFP, -log10(p_val_adj))) + 
     geom_point(aes(color = annot)) + xlim(c(-lim, lim)) + 
     geom_hline(yintercept = -log10(padj), linetype = 2) +
     geom_vline(xintercept = c(-logFC, logFC), linetype = 2) +
@@ -126,3 +128,4 @@ plot_FPcur = function(df, logFC = 1, padj = .01, exprAvg = 0, title = 'DEG FC-Pa
     theme_bw() + theme(text = element_text(family = 'serif', size = 14), plot.title = element_text(hjust = .5))  
   list(plot = p, df = df)
 }
+
